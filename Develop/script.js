a09bf77f8541c26e666c3aa47fdc11ccbe0ab231
Khai Truong <kminht@gmail.com>
2022-03-09 16:44:12 -0500

    bug fix

--- Develop/script.js
+++ Develop/script.js
@@ -5,49 +5,50 @@
 var generatePassword =function(){
  debugger;
 
-  var passLen = parseInt(window.prompt ("How long will the password be ?"));
+  let passLen = parseInt(window.prompt ("How long will the password be ?"));
   if (passLen >= 8 && passLen <= 128 ){
         console.log(passLen) // testing delete later
+
+        let isLower = window.confirm("Do you want lowercase?")
+        let isUpper = window.confirm("Do you want Uppercase?") 
+        let isNum = window.confirm("Do you want numerical values?")
+        let isSpecial = window.confirm("Do you want Special cases?")
+      
+      
+        //clear temp and pass storage
+        let temp = '';
+        let password = '';
+      
+        // check 
+        if(isLower){
+          temp += 'abcdefghijklmnopqrstuvwxyz'
+        }
+        if(isUpper){
+          temp += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        }
+        if(isNum){
+          temp += '0123456789'
+        }
+        if(isSpecial){
+          temp += '!@#$%^&*()?'
+        }
+      
+        for(let i = 0; i < passLen; i++){
+          // temp [] is now a arry with stored info from above 
+          // loop temp arry by the length of passLen
+          // random number temp arry will get stored into password untill loop stops
+          password += temp[ Math.floor( Math.random() * temp.length)]
+        }
+      
+        return password;
+
   }
   else{
     window.alert("password needs to be between 8 - 128 ");
     generatePassword();
   }
 
-  let isLower = window.confirm("Do you want lowercase?");
-  let isUpper = window.confirm("Do you want Uppercase?"); 
-  let isNum = window.confirm("Do you want numerical values?");
-  let isSpecial = window.confirm("Do you want Special cases?");
-
-
-  //clear temp and pass storage
-  let temp = '';
-  let password = '';
-
-  // check 
-  if(isLower){
-    temp += 'abcdefghijklmnopqrstuvwxyz'
-  }
-  if(isUpper){
-    temp += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-  }
-  if(isNum){
-    temp += '0123456789'
-  }
-  if(isSpecial){
-    temp += '!@#$%^&*()?'
-  }
-
-  for(let i = 0; i < passLen; i++){
-    // temp [] is now a arry with stored info from above 
-    // loop temp arry by the length of passLen
-    // random number temp arry will get stored into password untill loop stops
-    password += temp[ Math.floor( Math.random() * temp.length)]
-  }
-
-  return password;
-
-}
+};
 
 
 // Get references to the #generate element
